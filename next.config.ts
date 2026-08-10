a517b77465534db444c5943b70057ec37c8faf4e
calro999 <gamingmedia.mail@gmail.com>
2026-08-10 22:56:40 +0900

    更新

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // 静的エクスポート時には画像の自動最適化を無効化する
   },
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
+
+
